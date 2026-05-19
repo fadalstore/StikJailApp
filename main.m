10c2214b42c7365308edf2d45a4db2ef32a91cdf
fadalstore <fadalstore7@gmail.com>
2026-05-19 21:18:34 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,11 +1,12 @@
 #import <UIKit/UIKit.h>
 #import <unistd.h>
 #import <sys/stat.h>
+#import <dlfcn.h>
 
 @interface ViewController : UIViewController
 @property (strong, nonatomic) UITextView *logView;
 @property (strong, nonatomic) UIButton *injectButton;
-@property (strong, nonatomic) UIActivityIndicatorView *spinner;
+@property (strong, nonatomic) UILabel *statusLabel;
 @end
 
 @implementation ViewController
@@ -17,89 +18,98 @@
     CGFloat screenWidth = self.view.bounds.size.width;
     CGFloat screenHeight = self.view.bounds.size.height;
     
-    // Title
-    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, screenWidth, 40)];
-    titleLabel.text = @"StikJail v1.2";
-    titleLabel.textColor = [UIColor greenColor];
-    titleLabel.font = [UIFont boldSystemFontOfSize:26];
-    titleLabel.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:titleLabel];
+    // Header
+    UILabel *header = [[UILabel alloc] initWithFrame:CGRectMake(0, 40, screenWidth, 60)];
+    header.text = @"STIKJAIL EXPLOIT";
+    header.textColor = [UIColor whiteColor];
+    header.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:28];
+    header.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:header];
     
-    // Log View - Waxaan u yareeyay dhererka si badhanka loo arko
-    self.logView = [[UITextView alloc] initWithFrame:CGRectMake(15, 100, screenWidth - 30, screenHeight - 220)];
-    self.logView.backgroundColor = [UIColor colorWithWhite:0.05 alpha:1.0];
-    self.logView.textColor = [UIColor greenColor];
-    self.logView.font = [UIFont fontWithName:@"Courier" size:12];
+    // Status Label
+    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, screenWidth, 30)];
+    self.statusLabel.text = @"Ready to Inject";
+    self.statusLabel.textColor = [UIColor lightGrayColor];
+    self.statusLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.statusLabel];
+    
+    // Log View
+    self.logView = [[UITextView alloc] initWithFrame:CGRectMake(15, 130, screenWidth - 30, screenHeight - 250)];
+    self.logView.backgroundColor = [UIColor colorWithWhite:0.1 alpha:1.0];
+    self.logView.textColor = [UIColor cyanColor];
+    self.logView.font = [UIFont fontWithName:@"Menlo" size:11];
     self.logView.editable = NO;
-    self.logView.layer.cornerRadius = 10;
-    self.logView.layer.borderWidth = 1;
-    self.logView.layer.borderColor = [UIColor greenColor].CGColor;
-    self.logView.text = @"[+] System Ready...\n[+] Device: iOS SE (Detected)\n[+] UID: 501 (Sandbox)";
+    self.logView.layer.cornerRadius = 8;
+    self.logView.text = @"[i] StikJail initialized...\n[i] Waiting for user trigger...";
     [self.view addSubview:self.logView];
     
-    // Inject Button - Hadda wuxuu ka soo muuqanayaa shaashadda hoosteeda
+    // Inject Button
     self.injectButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.injectButton.frame = CGRectMake(screenWidth/2 - 100, screenHeight - 100, 200, 50);
-    self.injectButton.backgroundColor = [UIColor greenColor];
-    [self.injectButton setTitle:@"START INJECTION" forState:UIControlStateNormal];
+    self.injectButton.frame = CGRectMake(screenWidth/2 - 110, screenHeight - 100, 220, 55);
+    self.injectButton.backgroundColor = [UIColor whiteColor];
+    [self.injectButton setTitle:@"JAILBREAK" forState:UIControlStateNormal];
     [self.injectButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    self.injectButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
-    self.injectButton.layer.cornerRadius = 25;
-    [self.injectButton addTarget:self action:@selector(startInjection) forControlEvents:UIControlEventTouchUpInside];
+    self.injectButton.titleLabel.font = [UIFont boldSystemFontOfSize:20];
+    self.injectButton.layer.cornerRadius = 12;
+    [self.injectButton addTarget:self action:@selector(runExploit) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.injectButton];
-    
-    // Spinner
-    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
-    self.spinner.center = CGPointMake(screenWidth/2, screenHeight - 40);
-    self.spinner.color = [UIColor whiteColor];
-    [self.view addSubview:self.spinner];
 }
 
-- (void)addLog:(NSString *)text {
+- (void)log:(NSString *)msg {
     dispatch_async(dispatch_get_main_queue(), ^{
-        self.logView.text = [self.logView.text stringByAppendingFormat:@"\n%@", text];
+        self.logView.text = [self.logView.text stringByAppendingFormat:@"\n%@", msg];
         [self.logView scrollRangeToVisible:NSMakeRange(self.logView.text.length, 0)];
     });
 }
 
-- (void)startInjection {
+- (void)runExploit {
     self.injectButton.enabled = NO;
     self.injectButton.alpha = 0.5;
-    [self.spinner startAnimating];
+    self.statusLabel.text = @"Exploiting...";
     
-    [self addLog:@"[!] Billaabay Injection..."];
+    [self log:@"[!] Starting exploit chain..."];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        
-        // Baaritaanka SiriKitFlow sidii aad codsatay
-        [self addLog:@"[*] Checking SiriKitFlow access..."];
-        NSString *path = @"/System/Library/PrivateFrameworks/SiriKitFlow.framework/Info.plist";
-        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-            [self addLog:@"[+] Path Found: SiriKitFlow"];
+    // 1. Check SiriKitFlow
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC), dispatch_get_global_queue(0, 0), ^{
+        [self log:@"[*] Loading SiriKitFlow.framework..."];
+        void *h = dlopen("/System/Library/PrivateFrameworks/SiriKitFlow.framework/SiriKitFlow", RTLD_NOW);
+        if (h) {
+            [self log:@"[+] SiriKitFlow loaded successfully!"];
+            dlclose(h);
         } else {
-            [self addLog:@"[-] Error: Path restricted by Sandbox."];
+            [self log:@"[-] SiriKitFlow load failed (Sandbox)."];
         }
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [self addLog:@"[*] Exploiting Kernel (Simulated)..."];
+        // 2. Read Info.plist
+        [self log:@"[*] Reading SiriKitFlow Info.plist..."];
+        NSString *plistPath = @"/System/Library/PrivateFrameworks/SiriKitFlow.framework/Info.plist";
+        NSDictionary *plist = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+        if (plist) {
+            [self log:[NSString stringWithFormat:@"[+] Version: %@", plist[@"CFBundleShortVersionString"]]];
+        } else {
+            [self log:@"[-] Access Denied: Info.plist"];
+        }
+        
+        // 3. Simulated Kernel Tasks
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC), dispatch_get_global_queue(0, 0), ^{
+            [self log:@"[*] Searching for kernel slide..."];
+            [self log:@"[*] Found slide: 0x1a2b3c4d5e6f"];
+            [self log:@"[*] Patching setuid(0)..."];
             
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                [self addLog:@"[*] Overwriting task_self_addr..."];
-                
-                uid_t current_uid = getuid();
-                
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                    [self.spinner stopAnimating];
-                    if (current_uid == 0) {
-                        [self addLog:@"[+++] SUCCESS: ROOT ACCESS GRANTED!"];
-                        self.view.backgroundColor = [UIColor colorWithRed:0 green:0.2 blue:0 alpha:1];
-                    } else {
-                        [self addLog:@"[-] FAILED: Kernel patch failed."];
-                        [self addLog:@"[!] Device is still sandboxed."];
-                    }
-                    self.injectButton.enabled = YES;
-                    self.injectButton.alpha = 1.0;
-                });
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                uid_t u = getuid();
+                if (u == 0) {
+                    [self log:@"[!!!] ROOT ACCESS ACHIEVED!"];
+                    self.statusLabel.text = @"Jailbroken!";
+                    self.statusLabel.textColor = [UIColor greenColor];
+                } else {
+                    [self log:@"[-] Exploit failed (UID still 501)."];
+                    [self log:@"[!] Note: Real root needs a kernel exploit."];
+                    self.statusLabel.text = @"Exploit Failed";
+                    self.statusLabel.textColor = [UIColor redColor];
+                }
+                self.injectButton.enabled = YES;
+                self.injectButton.alpha = 1.0;
             });
         });
     });
